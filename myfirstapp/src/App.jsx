a79b33847994d767e7fe11d23b3deca8b32bef41
Aliza Khan <alizaadi2@gmail.com>
2024-08-27 21:00:56 +0500

    Update App.jsx

--- myfirstapp/src/App.jsx
+++ myfirstapp/src/App.jsx
@@ -1,11 +1,51 @@
 import logo from './logo.svg';
+import image from './assets/images/myimage.jpg'
 import './App.css';
 
+// const full_name = "Khan"
+//variables can be created inside or outside
+//functions must be created inside the fn App before return 
+
 function App() {
-  return (  
+  const full_name = "Khan"
+  const exampleFn = (name) => {
+    console.log("Parameter passed => ", name)
+  }
+  //list rendering 
+  const listRender = [
+    {
+      id: 6017,
+      name: "Aliza Khan",
+      age: 19,
+      field: "web developer"
+    },
+    {
+      id: 6020,
+      name: "Sara Faisal",
+      age: 22,
+      field: "flutter developer"
+    }
+  ]
+
+  const myArr = ["UBIT", " KUBS", " BBA ", 90]
+
+  return (
     <div className="App">
+      {/* Access array values */}
+      <h6>Array values = {myArr}</h6>
+      {/* ---------------------------------- */}
+
+      {/* Rendering list data here through JS map fn */}
+      <ul>
+        {
+          listRender.map((v, id) => <li key={id}>{v.name}</li>)
+        }
+      </ul>
+      {/* ---------------------------------- */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={image} className="App-image" alt="logo" />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,6 +57,11 @@ function App() {
         >
           Learn React
         </a>
+        <h1 style={{
+          backgroundColor: "green",
+          color: "blue"
+        }}>Aliza {full_name}</h1>
+        <button onClick={() => exampleFn("Adil khan")}>Click me</button>
       </header>
     </div>
   );
