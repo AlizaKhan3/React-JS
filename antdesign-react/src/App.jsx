c8fbb5fe263868e0d2f5c7b7dd8885f82d412ca9
Aliza Khan <alizaadi2@gmail.com>
2024-08-29 00:02:35 +0500

    Create App.jsx

---
+++ antdesign-react/src/App.jsx
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import AppNavbar from './components/navbar';
+import AppLayout from './components/layout';
+import Dashboard from './pages/dashboard';
+
+function App() {
+  return (
+    <div>
+      <Dashboard>
+        
+      </Dashboard>
+    </div>
+  );
+}
+
+export default App;
