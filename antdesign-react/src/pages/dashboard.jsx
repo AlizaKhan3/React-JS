e8cc83bece57d831f15597bea7d1b486a5098c7d
Aliza Khan <alizaadi2@gmail.com>
2024-08-29 00:02:52 +0500

    Create dashboard.jsx

---
+++ antdesign-react/src/pages/dashboard.jsx
@@ -0,0 +1,14 @@
+import AppLayout from "../components/layout";
+// import AppNavbar from "../components/navbar";
+function Dashboard() {
+    return (
+        <div>
+            {/* <AppNavbar>
+      
+      </AppNavbar> */}
+           <AppLayout></AppLayout>
+        </div>
+    )
+}
+
+export default Dashboard;
