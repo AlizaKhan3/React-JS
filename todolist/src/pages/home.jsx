b0a2bbff74f2d697021854965d048f74f571c3f8
Aliza Khan <alizaadi2@gmail.com>
2024-08-30 02:02:20 +0500

    Update home.jsx

--- todolist/src/pages/home.jsx
+++ todolist/src/pages/home.jsx
@@ -4,36 +4,61 @@ import { useState } from 'react'
 // import { Input } from "antd";
 import ClockCircleOutlined from '@ant-design/icons/ClockCircleOutlined';
 import { Input, Space } from 'antd';
-import React from 'react';
+import { SearchOutlined } from '@ant-design/icons';
 import { Button } from 'antd';
 
 function HomePage() {
-    const [todos, setTodos] = useState([{ value: "Aliza" }, { value: "Khan" }]);
-    const [value, setValue] = useState("")
-
+    const [todos, setTodos] = useState([{ value: "Aliza", disabled: true }, { value: "Khan", disabled: true }]);
+    const [value, setValue] = useState("");
+  
     const addTodo = () => {
-        setTodos([...todos, { value }])
+      setTodos([...todos, { value, disabled: true }]);
+      setValue("");
     }
-
+  
     return (
-        <div>
-            <Space
-                direction="vertical"
-                style={{
-                    width: '100%',
-                }}
-            >
-                <Input onChange={(e) => setValue(e.target.value)} status="warning" placeholder="Warning" />
-            </Space>
+      <div>
+        <Space
+          direction="vertical"
+          style={{
+            width: '100%',
+          }}
+        >
+          <Input value={value} onChange={(e) => setValue(e.target.value)} status="warning" placeholder="Warning" />
+        </Space>
+  
+        <Button onClick={() => addTodo()} type="primary">Primary Button</Button>
+        <Button onClick={() => setTodos([])} type="primary">Primary Button</Button>
 
-            <Button onClick={() => addTodo()} type="primary">Primary Button</Button>
-
-            <br />
-            <ul>
-                {todos.map((v, i) => <li key={i}> {v.value}</li>)}
-            </ul>
-        </div>
+        <br />
+  
+        <ul>
+          {todos.map((v, i) => (
+            <li key={i}>
+              <Input disabled={v.disabled} defaultValue={v.value} onChange={(e) => v.value = e.target.value} status="warning" placeholder="Warning" />
+  
+              <Button onClick={() => {
+                const oldTodos = [...todos];
+                oldTodos.splice(i, 1);
+                setTodos(oldTodos);
+              }} type="primary" shape="circle" icon={<SearchOutlined />} />
+  
+              {v.disabled ?
+                <Button type="primary" shape="circle" icon={<SearchOutlined />} onClick={() => {
+                  todos.splice(i, 1, { ...v, disabled: false });
+                  setTodos([...todos]);
+                }}> Edit</Button>
+              :
+                <Button type="primary" shape="circle" onClick={() => {
+                    v.disabled = true;
+                    setTodos([...todos]);
+                }}>Update</Button>
+              }
+            </li>
+          ))}
+        </ul>
+      </div>
     );
-}
-
-export default HomePage;
+  }
+  
+  export default HomePage;
