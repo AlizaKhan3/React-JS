fbb7ea0e4299b274f17cbe5b7780c35a8c4404f3
Aliza Khan <alizaadi2@gmail.com>
2024-08-30 00:23:44 +0500

    todo files

---
+++ todolist/src/components/button.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Button, Flex } from 'antd';
+
+const AppButton = () => (
+      <><Button type="primary">Primary</Button><Button type="primary" disabled>
+        Primary(disabled)
+    </Button></>
+);
+export default AppButton;
