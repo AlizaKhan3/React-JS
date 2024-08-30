f2d459db7276850b9771083a8f9ed086b0e52762
Aliza Khan <alizaadi2@gmail.com>
2024-08-31 00:45:25 +0500

    Create App.jsx

---
+++ login-signup-react/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import AppBasicForm from './components/form';
+import { Flex } from "antd";
+// import ValidationTextFields from './components/input';
+
+function App() {
+  return (
+    <div className="container">
+      {/* <ValidationTextFields></ValidationTextFields> */}
+    <AppBasicForm></AppBasicForm>
+    
+    </div>
+  );
+}
+
+export default App;
