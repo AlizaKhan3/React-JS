50285030114d3346bbbbf918ddaad60080a6ab05
Aliza Khan <alizaadi2@gmail.com>
2024-09-04 22:18:02 +0500

    profile.jsx

---
+++ myfirstapp/src/pages/profile.jsx
@@ -0,0 +1,11 @@
+import "../App.css" 
+
+const Profile = () => {
+    return(
+        <div className="profile">
+            <h1>Profile Page</h1>
+        </div>
+    )
+}
+
+export default Profile;
