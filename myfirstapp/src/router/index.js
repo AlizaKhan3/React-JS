e30582f25902a02b36cc74b0f694d21c43383511
Aliza Khan <alizaadi2@gmail.com>
2024-09-05 01:12:17 +0500

    Update index.js

--- myfirstapp/src/router/index.js
+++ myfirstapp/src/router/index.js
@@ -1,29 +1,21 @@
 import { BrowserRouter, Router, Route, Routes } from "react-router-dom";
-import Home from "../pages/home";
-import Profile from "../pages/profile";
+import Login from "../pages/login"
+import Home from "../pages/home"
+import Profile from "../pages/profile"
+import AppLayout from "../components/navbar"
 
 const AppRouter = () => {
-    return(
+    return (
         <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/profile" element={<Profile />}></Route>
-        </Routes> 
-    </BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/login" element={<Login />}></Route>
 
+                <Route path="/profile" element={<AppLayout><Profile /></AppLayout>}></Route>
+            </Routes>
+        </BrowserRouter>
     )
 }
-
-// const AppRouter = () => {
-//     return (
-//       <BrowserRouter>
-//         <Routes>
-//           <Route path="/home" element={<Home />}></Route>
-//           <Route path="/profile" element={<Profile />}></Route>
-//         </Routes>
-//       </BrowserRouter>
-//     );
-//   };
 
 
 export default AppRouter;
